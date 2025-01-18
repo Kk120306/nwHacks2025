b1db5433588a42ac1895d5264f4a2dcc5da30242
Sediupled <chaitanya.rook@gmail.com>
2025-01-18 14:46:30 -0800

    Person added

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Person {
+    float locationX;
+    float locationY;
+    int xp;
+    int points;
 
+    public Person(){
+        this.xp = 0;
+        this.points = 0;
+    }
+
+    void updateLocation() {
+        //live updates location
+    }
+
+    void findCache() {
+
+    }
+
+    void displayDirections(){
+        
+    }
 }
