65a6850e770d4e56000d0835d170db2bf8240d75
Sediupled <chaitanya.rook@gmail.com>
2025-01-18 19:04:42 -0800

    doggydog

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import SplashScreen from './components/SplashScreen.jsx'
 import './index.css'
 import HomeScreen from './components/HomeScreen.tsx'
-import Login from './components/Login.js'
+import Login from './components/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
