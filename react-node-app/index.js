4fe4a80341b45d9d727b888123786c3520d718af
Sediupled <chaitanya.rook@gmail.com>
2025-01-18 18:24:46 -0800

    index + app

--- react-node-app/index.js
+++ react-node-app/index.js
@@ -1,12 +1,21 @@
 const express = require("express")
+const ysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+})
 
 app.get("/api", (req, res) => {
     res.json ({fruits: ["apple", "orange"]})
 })
 
-app.listen (8080, () => {
-    console.log (`Server listening on 8080`);
+app.listen (8081, () => {
+    console.log (`Server listening on 8081`);
 });
 
