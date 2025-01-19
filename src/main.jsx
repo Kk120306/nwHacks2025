cd70acff061cc808ddacd97af6c33e37b0e7c10c
Kai Kameyama <1203kk06@gmail.com>
2025-01-18 17:54:49 -0800

    zaya wade

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import SplashScreen from './components/SplashScreen.jsx'
 import './index.css'
 import HomeScreen from './components/HomeScreen.tsx'
+import Login from './components/Login.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <SplashScreen /> */}
     <HomeScreen />
+    <Login />
   </React.StrictMode>,
 )
