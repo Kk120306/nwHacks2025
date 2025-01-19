34a6599ad439996d8f444784bada4305f8b974da
Kai Kameyama <1203kk06@gmail.com>
2025-01-18 19:37:00 -0800

    fixed the user state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'
+import axios from 'axios';
 
-function handleSubmit(event) {
-    event.preventDefault();
-    axios.post('http://localhost:8081/login', {email, password})
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+function Login() {
+    const [email, setEmail] = useState(''); // Corrected to useState
+    const [password, setPassword] = useState(''); // Corrected to useState
 
-function Login(){
-    const [email, setEmail] = userState('');
-    const [password, setPassword] = userState('');
-    function handleSubmit(event){
+    function handleSubmit(event) {
         event.preventDefault();
-        
+        axios.post('http://localhost:8081/login', { email, password })
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
-    return(
+    return (
         <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
             <div className='p-3 bg-white w-25'>
                 <form onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" placeholder='Enter Email' className='form-control'
-                        onChange={e => setEmail(e.target.value)}/>
+                        <input
+                            type="email"
+                            placeholder='Enter Email'
+                            className='form-control'
+                            onChange={e => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" placeholder='Enter Password' className='form-control'
-                        onChange={e => setPassword(e.target.value)}/>
+                        <input
+                            type="password"
+                            placeholder='Enter Password'
+                            className='form-control'
+                            onChange={e => setPassword(e.target.value)}
+                        />
                     </div>
                     <button className='btn btn-success'>Login</button>
                 </form>
             </div>
         </div>
-    )   
+    );
 }
 
 export default Login;
