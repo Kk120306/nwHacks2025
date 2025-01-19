8e185246e04dd23f7321618d86b3a8b29cfde659
Sediupled <chaitanya.rook@gmail.com>
2025-01-18 19:13:48 -0800

    ifjief

--- react-node-app/index.js
+++ react-node-app/index.js
@@ -9,8 +9,20 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
+    database: "crud"
+})
+app.post('/login', (req,res) => {
+    const sql = "SELECT * FROM login WHERE username = ? AND password = ?";
+    const values = [
+        req.body.email,
+        req.body.password
+    ]
 })
 
+db.query(sql, [values],(err,data) => {
+    if(err) return res.json("Login Failed");
+    return res.json(data);
+})
 app.get("/api", (req, res) => {
     res.json ({fruits: ["apple", "orange"]})
 })
