225d6259b0e1df74877474ecc53d88ed5f21c87b
Kai Kameyama <1203kk06@gmail.com>
2025-01-18 18:32:26 -0800

    fent

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,17 +2,26 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login(){
+    const [email, setEmail] = userState('');
+    const [password, setPassword] = userState('');
+    function handleSubmit(event){
+        event.preventDefault();
+        
+    }
+
     return(
-        <div className='d-flex justify-content-center align-items-center bg-primary'>
+        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
             <div className='p-3 bg-white w-25'>
-                <form action=" ">
+                <form onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" placeholder='Enter Email' className='form-control'/>
+                        <input type="email" placeholder='Enter Email' className='form-control'
+                        onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" placeholder='Enter Password' className='form-control'/>
+                        <input type="password" placeholder='Enter Password' className='form-control'
+                        onChange={e => setPassword(e.target.value)}/>
                     </div>
                     <button className='btn btn-success'>Login</button>
                 </form>
